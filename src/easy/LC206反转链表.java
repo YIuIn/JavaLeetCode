b3bbca9ec8b0d9d6f2b5131f59811abfbac1018a
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-07-15 01:58:57 +0800

    更新今日力扣刷题记录与思考过程 448.找到所有数组中消失的数字 485.最大连续1的个数

---
+++ src/easy/LC206反转链表.java
@@ -0,0 +1,16 @@
+package easy;
+
+public class LC206反转链表 {
+/*    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode filp=null;
+            while(head!=null){
+                ListNode next=head.next;
+                head.next=filp;
+                filp=head;
+                head=next;
+            }
+            return filp;
+        }
+    }*/
+}
---
+++ src/medium/LC092反转链表二.java
@@ -0,0 +1,19 @@
+package medium;
+
+public class LC092反转链表二 {
+    /*ListNode dummyNode =new ListNode(-1);
+    dummyNode.next=head;
+    ListNode pre=dummyNode;
+        for(int i=0;i<left-1;i++){
+        pre=pre.next;
+    }
+    ListNode cur=pre.next;
+    ListNode next;
+        for(int i=0;i<right-left+1;i++){
+        next=cur.next;
+        cur.next=next.next;
+        next.next=pre.next;
+        pre.next=next;
+    }
+        return dummyNode.next;*/
+}
