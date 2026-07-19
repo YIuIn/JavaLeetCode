e92106fb90891c5b8d60f66ae901ffc583071514
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-07-19 23:57:34 +0800

    更新今天的力扣刷题记录与思考过程

---
+++ src/medium/LC062不同路径.java
@@ -0,0 +1,21 @@
+package medium;
+
+public class LC062不同路径 {
+/*    class Solution {
+        public int uniquePaths(int m, int n) {
+            int [][] f=new int[m][n];
+            for(int i=0;i<m;i++){
+                f[i][0]=1;
+            }
+            for(int j=0;j<n;j++){
+                f[0][j]=1;
+            }
+            for(int i=1;i<m;i++){
+                for(int j=1;j<n;j++){
+                    f[i][j]=f[i-1][j]+f[i][j-1];
+                }
+            }
+            return f[m-1][n-1];
+        }
+    }*/
+}
