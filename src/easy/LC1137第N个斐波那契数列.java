2be1c8d9cdf3ba3fe23b18065d4e1ede64788752
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-07-22 02:30:58 +0800

    更新今天的力扣刷题记录与思考过程

---
+++ src/easy/LC1137第N个斐波那契数列.java
@@ -0,0 +1,17 @@
+package easy;
+
+public class LC1137第N个斐波那契数列 {
+   /* class Solution {
+        public int tribonacci(int n) {
+            if(n==0)return 0;
+            else if(n<=2)return 1;
+            int[] f=new int[n+1];
+            f[0]=0;
+            f[1]=f[2]=1;
+            for(int i=3;i<=n;i++){
+                f[i]=f[i-1]+f[i-2]+f[i-3];
+            }
+            return f[n];
+        }
+    }*/
+}
---
+++ src/medium/LC279完全平方数.java
@@ -0,0 +1,17 @@
+package medium;
+
+public class LC279完全平方数 {
+    /*class Solution {
+        public int numSquares(int n) {
+            int[] f = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                int minn = Integer.MAX_VALUE;
+                for (int j = 1; j * j <= i; j++) {
+                    minn = Math.min(minn, f[i - j * j]);
+                }
+                f[i] = minn + 1;
+            }
+            return f[n];
+        }
+    }*/
+}
