24d797cfa1bde4a7a534e2482ebc5a3713c00361
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-07-31 02:13:45 +0800

    更新今日力扣题

---
+++ src/medium/LC024两两交换链表中的节点.java
@@ -0,0 +1,20 @@
+package medium;
+
+public class LC024两两交换链表中的节点 {
+/*    class Solution {
+        public ListNode swapPairs(ListNode head) {
+            ListNode h0=new ListNode(0);
+            h0.next=head;
+            ListNode h=h0;
+            while(h0.next!=null&&h0.next.next!=null){
+                ListNode h1=h0.next;
+                ListNode h2=h0.next.next;
+                h0.next=h2;
+                h1.next=h2.next;
+                h2.next=h1;
+                h0=h1;
+            }
+            return h.next;
+        }
+    }*/
+}
