42c2f42e24919de60aabd712e0959a361804ea65
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-08-01 03:07:39 +0800

    更新今日力扣题

---
+++ src/medium/LC045跳跃游戏二.java
@@ -0,0 +1,20 @@
+package medium;
+
+public class LC045跳跃游戏二 {
+/*    class Solution {
+        public int jump(int[] nums) {
+            int max=0;
+            int end=0;
+            int count=0;
+            int n=nums.length;
+            for(int i=0;i<n-1;i++){
+                max=Math.max(max,i+nums[i]);
+                if(i==end){
+                    end=max;
+                    count++;
+                }
+            }
+            return count;
+        }
+    }*/
+}
---
+++ src/medium/LC055跳跃游戏.java
@@ -0,0 +1,16 @@
+package medium;
+
+public class LC055跳跃游戏 {
+/*    class Solution {
+        public boolean canJump(int[] nums) {
+            int n=nums.length;
+            int max=n-1;
+            for(int i=max-1;i>=0;i--){
+                if(i+nums[i]>=max)max=i;
+            }
+            if(max==0)return true;
+            return false;
+        }
+    }*/
+
+}
