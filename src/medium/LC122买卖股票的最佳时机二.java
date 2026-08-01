3dfe72eaee4f84d86dc2d87e5fdb8809b235378c
bsqe <294109836+YIuIn@users.noreply.github.com>
2026-08-02 03:11:18 +0800

    更新今日力扣题

---
+++ src/medium/LC122买卖股票的最佳时机二.java
@@ -0,0 +1,17 @@
+package medium;
+
+public class LC122买卖股票的最佳时机二 {
+/*    class Solution {
+        public int maxProfit(int[] prices) {
+            int min=prices[0];
+            int sum=0;
+            for(int i:prices){
+                if(i>min){
+                    sum+=i-min;
+                }
+                min=i;
+            }
+            return sum;
+        }
+    }*/
+}
---
+++ src/medium/LC763划分字母区间.java
@@ -0,0 +1,26 @@
+package medium;
+
+public class LC763划分字母区间 {
+/*
+    class Solution {
+        public List<Integer> partitionLabels(String s) {
+            int[] last = new int[26];
+            int length = s.length();
+            for (int i = 0; i < length; i++) {
+                last[s.charAt(i) - 'a'] = i;
+            }
+            List<Integer> partition = new ArrayList<Integer>();
+            int start = 0, end = 0;
+            for (int i = 0; i < length; i++) {
+                end = Math.max(end, last[s.charAt(i) - 'a']);
+                if (i == end) {
+                    partition.add(end - start + 1);
+                    start = end + 1;
+                }
+            }
+            return partition;
+        }
+    }
+
+*/
+}
